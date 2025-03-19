f9150419b9b13041c5021a4ea21110bf15fa7152
董飞扬 <dongfeiyang@dongfeiyangdeMacBook-Pro.local>
2025-03-19 13:44:04 +0800

    First

---
+++ Practical5/language.py
@@ -0,0 +1,4 @@
+Language=['JavaScript','HTML','Python','SQL','TypeScript']
+Users=['62.3%','52.9%','51%','51%','38.5%']
+data_dict=dict(zip(Language,Users))   
+print(data_dict)                                                         
