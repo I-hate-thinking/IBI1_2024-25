abf4cc8af3bbc4b4fc50d3a1c1a174630cbbbff5
董飞扬 <dongfeiyang@dongfeiyangdeMacBook-Pro.local>
2025-03-21 11:49:56 +0800

    second

--- Practical5/population.py
+++ Practical5/population.py
@@ -6,3 +6,20 @@ sorted_Zhejiang=sorted(around_Zhejiang)
 #use sorted()to sort the data
 print("After sorted, the population is",sorted_uk,"in uk, and",sorted_Zhejiang,"around Zhejiang")
 #print the result
+
+import matplotlib.pyplot as plt
+labels_uk=['England','Wales','Northern Ireland','Scotland']
+labels_Zhejiang=['Zhejiang','Fujian','Jiangxi','Anhui','Jiangsu']
+plt.figure(figsize=(6,6))
+plt.pie(uk_countries,labels=labels_uk,autopct='%1.11f%%',startangle=0)
+#
+plt.axis('equal')
+#
+plt.title(' the distribution of population sizes separately in UK countries')
+#add the title
+plt.show()
+plt.figure(figsize=(6,6))
+plt.pie(around_Zhejiang,labels=labels_Zhejiang,autopct='%1.11f%%',startangle=0)
+plt.axis('equal')
+plt.title(' the distribution of population sizes separately in Zhejiang-neighbouring provinces')
+plt.show()
