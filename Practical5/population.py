93dcbc55170fd44903a9edb02b23f5d6d00cce16
董飞扬 <dongfeiyang@dongfeiyangdeMacBook-Pro.local>
2025-03-21 11:21:14 +0800

    first

---
+++ Practical5/population.py
@@ -0,0 +1,8 @@
+uk_countries=[57.11,3.13,1.91,5.45]
+around_Zhejiang=[65.77,41.88,45.28,61.27,85.15]
+#store the population sizes in two lists 
+sorted_uk=sorted(uk_countries)
+sorted_Zhejiang=sorted(around_Zhejiang)
+#use sorted()to sort the data
+print("After sorted, the population is",sorted_uk,"in uk, and",sorted_Zhejiang,"around Zhejiang")
+#print the result
