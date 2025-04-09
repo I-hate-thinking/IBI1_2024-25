695a8db4bac8196fcc150f7c71b4dd48ebd446e6
董飞扬 <dongfeiyang@dongfeiyangdeMacBook-Pro.local>
2025-04-09 13:54:06 +0800

    /final

--- Practical8/patients.py
+++ Practical8/patients.py
@@ -8,14 +8,12 @@ class Patient:
     def print_details(self): # print out all the detailed information of the patient
         print(f"patients: {self.name}, age: {self.age}, the date of recent hospitalization: {self.admission_date}, medical history: {self.medical_history}")
 
-
 # example
-if __name__ == "__main__":
-    # create two example
+if __name__ == "__main__":  
     patient1 = Patient("Tom", 35, "2023-05-15", "diabetes")
-    patient2 = Patient("Amy", 28, "2023-06-20", "asthma")
-    
-    # print the information 
+    patient2 = Patient("Amy", 28, "2023-06-20", "asthma") 
+    # create two example
     print("hospital patient records:")
     patient1.print_details()
     patient2.print_details()
+    # print the information 
